a6ad924e2e4b0f332d3f324404149f060f0ecec7
igoodwinTechtonic <ian.goodwin@techtonic.com>
2020-10-27 14:20:31 -0500

    Module 4 tests

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -230,7 +230,9 @@ const getProduct = (prodList, pId) => {
 
 
 // Complete this function
-const calculateBill = (prod, tBill) => {};
+const calculateBill = (prod, tBill) => {
+    return prod.price + tBill;
+};
 
 const findPointsToBill = (roundedTotal) => {
     if (roundedTotal > 10 && roundedTotal <= 100) {
@@ -252,12 +254,24 @@ const findPointsToBill = (roundedTotal) => {
 
 
 // Complete this function
-const findPointsForExpDate = (prod) => {};
+const findPointsForExpDate = (prod) => {
+    if (prod.daysToExpire < 30) { return 10; }
+    else { return 0; }
+};
 
 
 const calculatePoints = (prod, tBill) => {
     let pointsToBill = findPointsToBill(Math.round(tBill));
     let pointsForExpDate = findPointsForExpDate(prod);
+    player.score = player.score + pointsToBill + pointsForExpDate;
+    if (prod instanceof MagicProduct) {
+        if (prod.hasOwnProperty('isBonus')) {
+            player.addPoints(prod);
+        }
+        else {
+            player.deductPoints(prod);
+        }
+    }
 };
 
 // Complete this function
